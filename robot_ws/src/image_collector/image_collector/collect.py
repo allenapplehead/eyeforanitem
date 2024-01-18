57d0a83951dd7af6d239eade292554c48045c5f6
Allen Tao <allentao7@gmail.com>
2024-01-18 18:13:10 -0500

    add save image feature

--- robot_ws/src/image_collector/image_collector/collect.py
+++ robot_ws/src/image_collector/image_collector/collect.py
@@ -1,4 +1,6 @@
 import cv2
+import os
+import datetime
 import rclpy
 import rclpy.node
 
@@ -9,13 +11,26 @@ class ImageCollector(rclpy.node.Node):
         # Declare parameters
         self.declare_parameter("capture_period", 1.0)  # Default values
         self.declare_parameter("dataset_dir", "/data/datasets/image_collector/train")
+        self.declare_parameter("save_imgs", True)
 
         # Get parameters
         self.capture_period_ = self.get_parameter("capture_period").get_parameter_value().double_value
         self.dataset_dir_ = self.get_parameter("dataset_dir").get_parameter_value().string_value
+        self.save_imgs_ = self.get_parameter("save_imgs").get_parameter_value().bool_value
 
+        print("+--------------------------+")
         print("Setting capture period to:", self.capture_period_)
         print("Setting dataset directory to:", self.dataset_dir_)
+        print("Save images:", self.save_imgs_)
+        print("+--------------------------+")
+
+        # Create dataset directory if it doesn't exist
+        if not os.path.exists(self.dataset_dir_):
+            try:
+                os.makedirs(self.dataset_dir_)
+                self.get_logger().info(f"Created dataset directory: {self.dataset_dir_}")
+            except OSError as e:
+                self.get_logger().error(f"Failed to create dataset directory: {self.dataset_dir_}. Error: {e}")
 
         # Initialize video capture
         self.cap = cv2.VideoCapture(0)
@@ -31,6 +46,14 @@ class ImageCollector(rclpy.node.Node):
         if not ret:
             self.get_logger().error('Failed to capture frame')
             return
+
+        # Check if images should be saved
+        if self.save_imgs_:
+            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+            filename = f"{timestamp}.png"
+            filepath = os.path.join(self.dataset_dir_, filename)
+            cv2.imwrite(filepath, frame)
+            self.get_logger().info(f"Saved image {filename}")
 
         # Display the current frame
         cv2.imshow('Current Frame', frame)
